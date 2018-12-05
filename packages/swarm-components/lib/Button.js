3268185f4d760fdb6eb1d839f105fe8eb319e45d
hsbacot <hsbacot@gmail.com>
2018-12-05 10:26:18 -0500

    node modules ignored in eslint, lerna repos linked, button wip

--- packages/swarm-components/lib/Button.js
+++ packages/swarm-components/lib/Button.js
@@ -2,10 +2,45 @@ function _extends() { _extends = Object.assign || function (target) { for (var i
 
 import * as React from 'react';
 
-function Button(props) {
+/**
+ *
+ * @param {*} props
+ *
+ */
+export const getButtonType = props => {
+  let buttonType = 'default';
+  if (props.disabled) buttonType = 'disabled';
+  if (props.primary) buttonType = 'primary';
+  if (props.neutral) buttonType = 'neutral';
+  if (props.bordered) buttonType = 'bordered';
+  if (props.inverted) buttonType = 'inverted';
+  return buttonType;
+};
+export const getSwarmSize = props => {
+  let size = 'default';
+  if (props.small) size = 'small';
+  return size;
+};
+
+const Button = props => {
+  // destructuring to not pass invalid attributes to node
+  const {
+    primary,
+    neutral,
+    bordered,
+    inverted,
+    small,
+    ...rest
+  } = props;
+
+  if (props.disabled) {
+    delete props.onClick;
+  }
+
   return React.createElement("button", _extends({
-    "data-swarm-button": "primary"
-  }, props));
-}
+    "data-swarm-button": getButtonType(props),
+    "data-swarm-size": getSwarmSize(props)
+  }, rest));
+};
 
 export default Button;
