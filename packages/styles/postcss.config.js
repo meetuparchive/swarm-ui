108d0ecd50d615e1d16dee2b2547ed96765b96ed
hsbacot <hsbacot@gmail.com>
2018-12-05 10:21:23 -0500

    updated styles for button variants

--- packages/styles/postcss.config.js
+++ packages/styles/postcss.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "plugins": [
+  plugins: [
     require("postcss-import"),
+    require("postcss-extend-rule"),
     require("autoprefixer"),
-    require("cssnano"),
+    require("cssnano")
   ],
-  "input": "lib/main.css",
-  "output": "dist/main.css",
-  "autoprefixer": {
-    "browsers": "> 5%",
+  input: "lib/main.css",
+  output: "dist/main.css",
+  autoprefixer: {
+    browsers: "> 5%"
   }
-}
+};
