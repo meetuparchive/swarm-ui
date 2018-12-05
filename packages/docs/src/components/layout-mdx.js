b153b79253e1930e5c01c2a17f4d99cec697b4d5
hsbacot <hsbacot@gmail.com>
2018-12-05 10:23:03 -0500

    registering swarm-components to be available within react-live

--- packages/docs/src/components/layout-mdx.js
+++ packages/docs/src/components/layout-mdx.js
@@ -1,34 +1,32 @@
 import React from 'react'
 import Layout from './layout'
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 import { MDXProvider } from '@mdx-js/tag'
 
+// imports for use in react-live
+import * as scope from 'swarm-components'
 
 const PreComponent = ({ className, ...props }) => {
-
   return props.children.props.props &&
-  props.children.props.props.className ===
-    "language-.jsx" ? (
-    <LiveProvider
-      code={props.children.props.children}
-    >
+    props.children.props.props.className === 'language-.jsx' ? (
+    <LiveProvider code={props.children.props.children} scope={scope}>
       <LiveEditor tabIndex="-1" />
       <LiveError />
       <LivePreview />
     </LiveProvider>
   ) : (
     <pre {...props} className="pre-static" />
-  );
-};
+  )
+}
 
 export default class MDXLayout extends React.Component {
   render() {
     return (
-        <Layout>
-            <MDXProvider components={{ pre: PreComponent }}>
-              {this.props.children}
-            </MDXProvider>
-        </Layout>
+      <Layout>
+        <MDXProvider components={{ pre: PreComponent }}>
+          {this.props.children}
+        </MDXProvider>
+      </Layout>
     )
   }
 }
