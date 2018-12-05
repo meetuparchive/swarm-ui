cdd9bf71571b7d387fdd6d0ee334396cd1906a93
hsbacot <hsbacot@gmail.com>
2018-12-05 10:24:13 -0500

    button types and size resolved

--- packages/swarm-components/src/Button.jsx
+++ packages/swarm-components/src/Button.jsx
@@ -2,11 +2,53 @@
 import * as React from 'react';
 
 type Props = {
+  primary?: boolean,
+  neutral?: boolean,
+  bordered?: boolean,
+  disabled?: boolean,
+  inverted?: boolean,
+  className?: string,
+  small?: boolean,
+  onClick?: (*) => void,
   children: React.Node
 };
 
-function Button(props: Props) {
-  return <button data-swarm-button="primary" {...props} />;
-}
+/**
+ *
+ * @param {*} props
+ *
+ */
+export const getButtonType = (props: Props): string => {
+  let buttonType = 'default';
+  if (props.disabled) buttonType = 'disabled';
+  if (props.primary) buttonType = 'primary';
+  if (props.neutral) buttonType = 'neutral';
+  if (props.bordered) buttonType = 'bordered';
+  if (props.inverted) buttonType = 'inverted';
+  return buttonType;
+};
+
+export const getSwarmSize = (props: Props): string => {
+  let size = 'default';
+  if (props.small) size = 'small';
+  return size;
+};
+
+const Button = (props: Props): React.Element<'button'> => {
+  // destructuring to not pass invalid attributes to node
+  const { primary, neutral, bordered, inverted, small, ...rest } = props;
+
+  if (props.disabled) {
+    delete props.onClick;
+  }
+
+  return (
+    <button
+      data-swarm-button={getButtonType(props)}
+      data-swarm-size={getSwarmSize(props)}
+      {...rest}
+    />
+  );
+};
 
 export default Button;
