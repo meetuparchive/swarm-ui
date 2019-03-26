36377a0ca10584214510395356bf46d0efca173b
hsbacot <hsbacot@gmail.com>
2019-03-26 13:35:00 -0400

    line height and color added

--- packages/swarm-components/lib/index.js
+++ packages/swarm-components/lib/index.js
@@ -1,5 +1,7 @@
 export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
+export { default as FieldHelper } from './FieldHelper';
+export { default as FieldLabel } from './FieldLabel';
 export { default as Icon } from './Icon';
 export { default as Radio } from './Radio';
 export { default as TextInput } from './TextInput';
