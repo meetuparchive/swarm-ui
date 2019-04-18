15be337ff5c924dd6619049bd617032c05b3ae55
hsbacot <hsbacot@gmail.com>
2019-04-18 19:50:50 -0400

    fix: including example

---
+++ packages/swarm-docs/src/components/textareaExample.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Textarea } from '@meetup/swarm-components';
+
+const TextareaExample = () => {
+  const [value, setValue] = React.useState('Swarm Textarea example');
+  const [value2, setValue2] = React.useState('Swarm Textarea example');
+
+  return (
+    <>
+      <Textarea value={value} onChange={e => setValue(e.target.value)} />
+      <br />
+      <p>with autosize</p>
+      <Textarea
+        value={value2}
+        onChange={e => setValue2(e.target.value)}
+        autosize
+      />
+    </>
+  );
+};
+
+export default TextareaExample;
