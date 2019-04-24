5c9effb2eb5b02ef40ad616622f042574ff66e14
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2019-04-24 11:19:58 -0400

    test: fixed body style configuration

--- packages/swarm-components/src/Button.test.jsx
+++ packages/swarm-components/src/Button.test.jsx
@@ -26,21 +26,74 @@ describe('Button', () => {
 	});
 
 	const testCases = [
-		['Default', <Button key="1" >Press me</Button>],
-		['Disabled', <Button disabled key="2">Can&apos;t press me</Button>],
-		['Primary', <Button primary key="3">Must press me</Button>],
-		['Bordered', <Button bordered key="4">Press me</Button>],
-		['Icon with text', <Button icon="bolt" key="5">Press me</Button>],
-		['Icon (right) with text', <Button right icon="chevron-right" key="6">Press me</Button>],
+		['Default', <Button key="1">Press me</Button>],
+		[
+			'Disabled',
+			<Button disabled key="2">
+				Can&apos;t press me
+			</Button>,
+		],
+		[
+			'Primary',
+			<Button primary key="3">
+				Must press me
+			</Button>,
+		],
+		[
+			'Bordered',
+			<Button bordered key="4">
+				Press me
+			</Button>,
+		],
+		[
+			'Icon with text',
+			<Button icon="bolt" key="5">
+				Press me
+			</Button>,
+		],
+		[
+			'Icon (right) with text',
+			<Button right icon="chevron-right" key="6">
+				Press me
+			</Button>,
+		],
 		['Icon without text', <Button icon="bolt" key="7" />],
-		['Reset', <Button reset key="8"> Press me</Button>],
-		['Inverted', <Button inverted key="9"> Press me</Button>, {bodyStyle: 'background-color:powderblue;'}],
-		['Neutral',<Button neutral key="10"> Press me</Button>],
-		['Small',<Button small key="11"> Press me</Button>],
+		[
+			'Reset',
+			<Button reset key="8">
+				{' '}
+				Press me
+			</Button>,
+		],
+		[
+			'Inverted',
+			<Button inverted key="9">
+				{' '}
+				Press me
+			</Button>,
+			{ bodyStyle: 'background-color: powderblue;' },
+		],
+		[
+			'Neutral',
+			<Button neutral key="10">
+				{' '}
+				Press me
+			</Button>,
+		],
+		[
+			'Small',
+			<Button small key="11">
+				{' '}
+				Press me
+			</Button>,
+		],
 	];
-	test.each(testCases)('Visual diff: %s', async (description, element) => {
-		expect(
-			await renderer.screenshot(element)
-		).toMatchImageSnapshot();
-	});
+	test.each(testCases)(
+		'Visual diff: %s',
+		async (description, element, screenshotConfig = {}) => {
+			expect(
+				await renderer.screenshot(element, screenshotConfig)
+			).toMatchImageSnapshot();
+		}
+	);
 });
