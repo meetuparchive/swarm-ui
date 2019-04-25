f2167a0b84d1a3473020bc160f2ed5536b95c865
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2019-04-25 16:21:22 -0400

    feat: added "did you run `yarn build`" note to error messages

--- packages/swarm-components/src/testUtils/prerequisites.js
+++ packages/swarm-components/src/testUtils/prerequisites.js
@@ -9,13 +9,17 @@ module.exports = async () => {
 			Path.join(__dirname, '../../../swarm-styles/dist/global.css')
 		)
 	) {
-		throw new Exception("Can't find packages/swarm-styles/dist/global.css");
+		throw new Exception(
+			"Can't find packages/swarm-styles/dist/global.css! Did you run `yarn build`?"
+		);
 	}
 
 	if (
 		!fs.existsSync(Path.join(__dirname, '../../../swarm-styles/dist/main.css'))
 	) {
-		throw new Exception("Can't find packages/swarm-styles/dist/main.css");
+		throw new Exception(
+			"Can't find packages/swarm-styles/dist/main.css! Did you run `yarn build`?"
+		);
 	}
 
 	if (
