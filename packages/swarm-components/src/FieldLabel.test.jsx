35b3333db3cb0057181723efab61ba0a302a9708
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2019-04-26 14:29:53 -0400

    feat: merged FieldLabel tests into single screenshot

--- packages/swarm-components/src/FieldLabel.test.jsx
+++ packages/swarm-components/src/FieldLabel.test.jsx
@@ -5,6 +5,8 @@ expect.extend({ toMatchImageSnapshot });
 
 import getScreenRenderer from './testUtils/screenRenderer';
 
+import { getTestMarkup } from './testUtils/testUtils.js';
+
 import FieldLabel from './FieldLabel';
 import Checkbox from './Checkbox';
 
@@ -14,7 +16,7 @@ describe('FieldLabel', () => {
 	// This is ran when the suite starts up.
 	beforeAll(async () => {
 		renderer = await getScreenRenderer({
-			viewport: { width: 300, height: 100 },
+			viewport: { width: 250, height: 200 },
 			// verbose: true,
 		});
 	});
@@ -25,24 +27,22 @@ describe('FieldLabel', () => {
 		return renderer.stop();
 	});
 
-	it('Label alone', async () => {
-		expect(
-			await renderer.screenshot(<FieldLabel>Field Label</FieldLabel>)
-		).toMatchImageSnapshot();
-	});
-
-	it('Pass focus', async () => {
-		const checkboxWithLabel = (
-			<div>
+	const testCases = [
+		['Label alone', <FieldLabel key="alone">Field Label</FieldLabel>],
+		[
+			'Pass focus',
+			<div key="focus">
 				<FieldLabel id="swarm-external-label" htmlFor="swarm-checkbox">
 					External checkbox Label
 				</FieldLabel>
 				<Checkbox id="swarm-checkbox">Checkbox&apos;s own text</Checkbox>
-			</div>
-		);
+			</div>,
+		],
+	];
 
+	it('Visually matches', async () => {
 		expect(
-			await renderer.screenshot(checkboxWithLabel, {
+			await renderer.screenshot(getTestMarkup(testCases), {
 				beforeScreenshot: page => {
 					page.click('#swarm-external-label');
 				},
