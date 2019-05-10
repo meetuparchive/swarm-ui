e65f056d5c9ebfbf44a7462f4141b01985a53837
Sadaf Shahsahebi <sadaf@meetup.com>
2019-05-10 08:49:03 -0700

    test: revert linkbutton.test.jsx to use jest snapshots

--- packages/swarm-components/src/LinkButton.test.jsx
+++ packages/swarm-components/src/LinkButton.test.jsx
@@ -1,31 +1,8 @@
 import React from 'react';
-
-import { toMatchImageSnapshot } from 'jest-image-snapshot';
-expect.extend({ toMatchImageSnapshot });
-
-import getScreenRenderer from './testUtils/screenRenderer';
+import { shallow } from 'enzyme';
 import LinkButton from './LinkButton';
-import { getTestMarkup } from './testUtils/testUtils.js';
 
 describe('LinkButton', () => {
-	let renderer;
-
-	// This is ran when the suite starts up.
-	beforeAll(async () => {
-		renderer = await getScreenRenderer({
-			// intentionally have a large viewport height
-			// to accomodate future states
-			viewport: { width: 300, height: 2000 },
-			// verbose: true,
-		});
-	});
-
-	// This is ran when the suite is done - stop the renderer.
-	afterAll(() => {
-		// comment next line out if you want to open it in your browser for debugging
-		return renderer.stop();
-	});
-
 	const testCases = [
 		['Default', <LinkButton key="1" >Press me</LinkButton>],
 		['Disabled', <LinkButton disabled key="2">Can&apos;t press me</LinkButton>],
@@ -34,16 +11,12 @@ describe('LinkButton', () => {
 		['Icon with text', <LinkButton icon="bolt" key="5">Press me</LinkButton>],
 		['Icon (right) with text', <LinkButton right icon="chevron-right" key="6">Press me</LinkButton>],
 		['Icon without text', <LinkButton icon="bolt" key="7" />],
-		['Inverted', <LinkButton inverted key="8"> Press me</LinkButton>, {backgroundColor: 'black', padding: '1em'}],
+		['Inverted', <LinkButton inverted key="8"> Press me</LinkButton>],
 		['Neutral', <LinkButton neutral key="9"> Press me</LinkButton>],
 		['Small', <LinkButton small key="10"> Press me</LinkButton>],
 		['Grow', <LinkButton grow key="11">Full Width Button</LinkButton>],
 	];
-
-	it('Default', async () => {
-		expect(
-			await renderer.screenshot(getTestMarkup(testCases))
-		).toMatchImageSnapshot();
+	test.each(testCases)('Snapshot: %s', (description, element) => {
+		expect(shallow(element)).toMatchSnapshot();
 	});
-
 });
