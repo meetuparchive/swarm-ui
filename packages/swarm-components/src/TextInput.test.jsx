5cf9687094c2fdf976868fe50414bec03a0144e5
Sadaf Shahsahebi <sadaf@meetup.com>
2019-05-29 15:34:32 -0700

    chore: run prettier on more files

--- packages/swarm-components/src/TextInput.test.jsx
+++ packages/swarm-components/src/TextInput.test.jsx
@@ -3,19 +3,77 @@ import { shallow } from 'enzyme';
 import TextInput from './TextInput';
 
 describe('TextInput', () => {
-    const mockOnChange = jest.fn();
+	const mockOnChange = jest.fn();
 
 	const testCases = [
-		['Default (empty)', <TextInput key="1" id="default" name="default" value="" />],
-		['Default (value)', <TextInput key="2" id="default" name="default" value="foo" />],
-		['Disabled', <TextInput key="3" id="disabled" name="disabled" value="" disabled />],
+		[
+			'Default (empty)',
+			<TextInput key="1" id="default" name="default" value="" />,
+		],
+		[
+			'Default (value)',
+			<TextInput key="2" id="default" name="default" value="foo" />,
+		],
+		[
+			'Disabled',
+			<TextInput key="3" id="disabled" name="disabled" value="" disabled />,
+		],
 		['Error', <TextInput key="4" id="error" name="error" value="" error />],
-		['isSearch', <TextInput key="5" id="isSearch" name="isSearch" value="" isSearch />],
-        ['Pattern', <TextInput key="6" id="pattern" name="pattern" value="" pattern="[A-Za-z]{3}" />],
-        ['onChange', <TextInput key="7" id="onChange" name="onChange" value="" onChange={mockOnChange} />],
-        ['Icon', <TextInput key="8" id="icon" name="icon" value="" iconShape="location-pin" />],
-        ['Icon (custom size)', <TextInput key="9" id="iconsize" name="iconsize" value="" iconShape="location-pin" iconSize="m" />],
-		['maxLength (char count)', <TextInput key="10" id="maxLength" name="maxLength" value="foo fooo" maxLength="20"/>],
+		[
+			'isSearch',
+			<TextInput key="5" id="isSearch" name="isSearch" value="" isSearch />,
+		],
+		[
+			'Pattern',
+			<TextInput
+				key="6"
+				id="pattern"
+				name="pattern"
+				value=""
+				pattern="[A-Za-z]{3}"
+			/>,
+		],
+		[
+			'onChange',
+			<TextInput
+				key="7"
+				id="onChange"
+				name="onChange"
+				value=""
+				onChange={mockOnChange}
+			/>,
+		],
+		[
+			'Icon',
+			<TextInput
+				key="8"
+				id="icon"
+				name="icon"
+				value=""
+				iconShape="location-pin"
+			/>,
+		],
+		[
+			'Icon (custom size)',
+			<TextInput
+				key="9"
+				id="iconsize"
+				name="iconsize"
+				value=""
+				iconShape="location-pin"
+				iconSize="m"
+			/>,
+		],
+		[
+			'maxLength (char count)',
+			<TextInput
+				key="10"
+				id="maxLength"
+				name="maxLength"
+				value="foo fooo"
+				maxLength="20"
+			/>,
+		],
 	];
 
 	test.each(testCases)('Snapshot: %s', (description, element) => {
