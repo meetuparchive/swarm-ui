c878a1c5cf67be63ff9e7cd0fd99f4749fb216e3
Sadaf Shahsahebi <sadaf@meetup.com>
2019-05-29 15:35:42 -0700

    chore: run prettier on text input .jsx

--- packages/swarm-components/src/TextInput.jsx
+++ packages/swarm-components/src/TextInput.jsx
@@ -1,104 +1,102 @@
 // @flow
 import * as React from 'react';
 import Icon from './Icon';
-import CharCount, {
-  getRemainingCharacters,
-} from './shared/CharCount';
+import CharCount, { getRemainingCharacters } from './shared/CharCount';
 
 const ICON_SIZES = Object.freeze({
-  XXS: 'xxs',
-  XS: 'xs',
-  S: 's',
-  M: 'm',
-  L: 'l',
-  XL: 'xl',
-  XXL: 'xxl',
+	XXS: 'xxs',
+	XS: 'xs',
+	S: 's',
+	M: 'm',
+	L: 'l',
+	XL: 'xl',
+	XXL: 'xxl',
 });
 
 type Props = {
-  /**
-   * Whether the input should be interactive.
-   */
-  disabled?: boolean,
-  /**
-   * Whether the field has an error.
-   */
-  error?: boolean,
-  /**
-   * An identifier for the input.
-   */
-  id: string,
-  /**
-   * Whether the input is a search field.
-   */
-  isSearch?: boolean,
-  /**
-   * Name for the input.
-   */
-  name: string,
-  /**
-   * A regular expression that the input's value is checked against on form submission.
-   */
-  pattern?: string,
-  /**
-   * Value of input.
-   */
-  value: string,
-  /**
-   * Name of icon to render in the input
-   */
-  iconShape?: string,
-  /**
-   * Optional size for icon if `iconShape` is provided.
-   * Default size is `xs`
-   */
-  iconSize?: $Values<typeof ICON_SIZES>,
-  /**
-   * max length of input field
-   */
-  maxLength?: string | number,
+	/**
+	 * Whether the input should be interactive.
+	 */
+	disabled?: boolean,
+	/**
+	 * Whether the field has an error.
+	 */
+	error?: boolean,
+	/**
+	 * An identifier for the input.
+	 */
+	id: string,
+	/**
+	 * Whether the input is a search field.
+	 */
+	isSearch?: boolean,
+	/**
+	 * Name for the input.
+	 */
+	name: string,
+	/**
+	 * A regular expression that the input's value is checked against on form submission.
+	 */
+	pattern?: string,
+	/**
+	 * Value of input.
+	 */
+	value: string,
+	/**
+	 * Name of icon to render in the input
+	 */
+	iconShape?: string,
+	/**
+	 * Optional size for icon if `iconShape` is provided.
+	 * Default size is `xs`
+	 */
+	iconSize?: $Values<typeof ICON_SIZES>,
+	/**
+	 * max length of input field
+	 */
+	maxLength?: string | number,
 };
 
 /**
  * @module TextInput
  */
 export const TextInput = (props: Props): React$Element<*> => {
-  const {
-    name,
-    error,
-    isSearch,
-    pattern,
-    disabled,
-    id,
-    iconShape,
-    iconSize = 'xs',
-    maxLength,
-    ...other
-  } = props;
+	const {
+		name,
+		error,
+		isSearch,
+		pattern,
+		disabled,
+		id,
+		iconShape,
+		iconSize = 'xs',
+		maxLength,
+		...other
+	} = props;
 
-  const wrapperState = iconShape ? 'icon' : 'default';
-  const inputState = disabled ? 'disabled' : error ? 'error' : 'default';
-  const remainingCharacters = getRemainingCharacters(maxLength, props.value);
+	const wrapperState = iconShape ? 'icon' : 'default';
+	const inputState = disabled ? 'disabled' : error ? 'error' : 'default';
+	const remainingCharacters = getRemainingCharacters(maxLength, props.value);
 
-  return (
-    <div data-swarm-text-input-wrapper={wrapperState}>
-      <input
-        data-swarm-text-input={inputState}
-        type={isSearch ? 'search' : 'text'}
-        name={name}
-        pattern={pattern}
-        disabled={disabled}
-        id={id}
-        {...other}
-      />
-      {iconShape && (
-        <span data-swarm-input-icon={iconShape}>
-          <Icon shape={iconShape} size={iconSize} aria-hidden={true} />
-        </span>
-      )}
-      {maxLength && <CharCount>{remainingCharacters}</CharCount>}
-    </div>
-  );
+	return (
+		<div data-swarm-text-input-wrapper={wrapperState}>
+			<input
+				data-swarm-text-input={inputState}
+				type={isSearch ? 'search' : 'text'}
+				name={name}
+				pattern={pattern}
+				disabled={disabled}
+				id={id}
+				{...other}
+			/>
+			{iconShape && (
+				<span data-swarm-input-icon={iconShape}>
+					<Icon shape={iconShape} size={iconSize} aria-hidden={true} />
+				</span>
+			)}
+			{maxLength && <CharCount>{remainingCharacters}</CharCount>}
+		</div>
+	);
 };
 
 export default TextInput;
