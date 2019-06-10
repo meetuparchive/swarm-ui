1b1fa5fa30329339a760bd4ce9735cee38b2f15b
hsbacot <hsbacot@gmail.com>
2019-06-10 18:29:37 -0400

    chore: upgrading to mdx v1 from v0

--- packages/swarm-docs/src/components/layout-mdx.js
+++ packages/swarm-docs/src/components/layout-mdx.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Layout from './layout'
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
-import { MDXProvider } from '@mdx-js/tag'
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Layout from './layout';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import { MDXProvider } from '@mdx-js/react';
 
 // imports for use in react-live
-import * as scope from '@meetup/swarm-components'
+import * as scope from '@meetup/swarm-components';
 
 const PreComponent = ({ className, ...props }) => {
-  return props.children.props.props &&
-    props.children.props.props.className === 'language-.jsx' ? (
+  return props.children.props &&
+    props.children.props.className === 'language-.jsx' ? (
     <LiveProvider code={props.children.props.children} scope={scope}>
       <LiveEditor tabIndex="-1" />
       <LiveError />
@@ -16,25 +17,25 @@ const PreComponent = ({ className, ...props }) => {
     </LiveProvider>
   ) : (
     <pre {...props} className="pre-static" />
-  )
-}
+  );
+};
 
 // apply swarm classnames to classes used in markdown
-const H1 = props => <h1 className="text--display" {...props}/>
-const H2 = props => <h2 className="text--pageTitle" {...props}/>
-const H3 = props => <h3 className="text--big" {...props}/>
-const H4 = props => <h3 className="text--sectionTitle" {...props}/>
-const P = props => <p className="text--body" {...props}/>
-const A = props => <a className="text--link" {...props}/>
+const He = props => <h1 className="text--display" {...props}/>;
+const H2 = props => <h2 className="text--pageTitle" {...props}/>;
+const H3 = props => <h3 className="text--big" {...props}/>;
+const H4 = props => <h3 className="text--sectionTitle" {...props}/>;
+const P = props => <p className="text--body" {...props}/>;
+const A = props => <a className="text--link" {...props}/>;
 
 export default class MDXLayout extends React.Component {
   render() {
     return (
       <Layout>
-        <MDXProvider components={{ pre: PreComponent, h1: H1, h2: H2, h3: H3, h4: H4, p: P, a: A }}>
+        <MDXProvider components={{ pre: PreComponent, h1: He, h2: H2, h3: H3, h4: H4, p: P, a: A }}>
           {this.props.children}
         </MDXProvider>
       </Layout>
-    )
+    );
   }
 }
