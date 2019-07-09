24b27aa9f56f2c18a503cd57f11295bd7d2ed13c
osermay <kristinm@meetup.com>
2019-07-09 10:44:59 -0400

    styling swatches

--- packages/swarm-docs/src/components/swatch.jsx
+++ packages/swarm-docs/src/components/swatch.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 
 const Swatch = props => {
-	const displayName = props.name.split("color").pop().split("-").join(" ")
+	const displayName = props.name.split("color").pop().split("-").join(" ");
+	const cssVarName = `--${props.name}`;
 	return (
-		<div onClick={() => navigator.clipboard.writeText(`--${props.name}`)} style={{ cursor: "pointer", backgroundColor: props.cssVariable, height:250, width: 200, margin: 15, "border-radius": 6}}>
-			<p style={{backgroundColor: "white", "margin-top": 150, "text-align": "center"}} >{`${displayName}`}</p>
+		<div onClick={() => navigator.clipboard.writeText(cssVarName)} style={{ cursor: "pointer", position: "relative", backgroundColor: props.cssVariable, height:239, width: 190, margin: 15, "border-radius": 6, "boxShadow": "0px 2px 5px 0px rgba(0, 0, 0, 0.1)", "border": "1px solid #f6f7f8"}}>
+			<div style={{ fontFamily: "AndaleMono", "font-size": 13, position: "absolute", bottom: 0, width: "100%",  backgroundColor: "#f6f7f8"}}>
+				<div style={{padding: 10}} >
+					<p>{`${displayName}`}</p>
+					<p>{props.cssVariable}</p>
+					<p>{cssVarName}</p>
+				</div>
+			</div>
 		</div>
 	);
 };
