e81fb1fd6781c6fda757e2144c679137aae0dbff
osermay <kristinm@meetup.com>
2019-07-09 11:20:37 -0400

    formatting

--- packages/swarm-docs/src/components/swatches.jsx
+++ packages/swarm-docs/src/components/swatches.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import colors from '@meetup/swarm-constants/build/js/colors';
 import Swatch from './swatch';
 
-const Swatches = props => {
- return(
- 	<div style={{display: "flex", "flexWrap": "wrap"}}>
-	 {colors.map(color => <Swatch cssVariable={color.color} name={color.name} />)}
-	 </div>);
-
-};
+const Swatches = props => (
+	<div style={{display: "flex", "flexWrap": "wrap"}}>
+		{colors.map(color => <Swatch cssVariable={color.color} name={color.name} />)}
+	</div>
+);
 export default Swatches;
-
