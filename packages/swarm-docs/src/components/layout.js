fb36265fcb90a468475c05b536cb9aca37dba7d7
hsbacot <hsbacot@gmail.com>
2019-08-07 15:45:07 -0400

    docs: site metadata updated in swarm-docs

--- packages/swarm-docs/src/components/layout.js
+++ packages/swarm-docs/src/components/layout.js
@@ -40,8 +40,8 @@ const Layout = ({ children }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Meetup\'s design system and react component library' },
+              { name: 'keywords', content: 'swarm design system, meetup, react design system' },
             ]}
           >
             <html lang="en" />
