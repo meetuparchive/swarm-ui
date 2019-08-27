177db301d8c22e6b1950f59d558fabba9ba3dbbf
hsbacot <hsbacot@gmail.com>
2019-08-27 11:34:06 -0400

    refactor: scoping index.js builds in icons

--- packages/swarm-icons/src/build.js
+++ packages/swarm-icons/src/build.js
@@ -37,40 +37,57 @@ if (!files.length) {
 	console.error('Could not list the directory.');
 	process.exit(1);
 }
-// initialize empty index file
-fs.writeFileSync(`${__dirname}/components/${argv.family}/index.js`, '');
 
-files
-	.sort((a, b) => toCamelCase(a).localeCompare(toCamelCase(b)))
-	.forEach(function(file, index) {
-		// syncronous in order to keep alphabetical order
-		const data = fs.readFileSync(`${__dirname}/icons/${argv.family}/${file}`);
-		let contents = data.toString();
+const alphabetizedFiles = files.sort((a, b) =>
+	toCamelCase(a).localeCompare(toCamelCase(b))
+);
 
-		// stripping outer svg tag
-		contents = contents
-			.replace(/<svg[^>]*>/, '')
-			.replace('</svg>', '')
-			.replace(/(\S)\/>/g, '$1 />')
-			.replace(/\sfill="#[^"]*"/g, '')
-			.replace(/fill-rule/g, 'fillRule');
+alphabetizedFiles.forEach(function(file, index) {
+	// syncronous in order to keep alphabetical order
+	const data = fs.readFileSync(`${__dirname}/icons/${argv.family}/${file}`);
+	let contents = data.toString();
 
+	// stripping outer svg tag
+	contents = contents
+		.replace(/<svg[^>]*>/, '')
+		.replace('</svg>', '')
+		.replace(/(\S)\/>/g, '$1 />')
+		.replace(/\sfill="#[^"]*"/g, '')
+		.replace(/fill-rule/g, 'fillRule');
+
+	const camelCaseFilename = toCamelCase(file);
+	const componentName =
+		camelCaseFilename.charAt(0).toUpperCase() + // TitleCasing
+		camelCaseFilename.slice(1).replace('.svg', ''); // stripping .svg extension
+
+	// write component JSX
+	fs.writeFileSync(
+		`${__dirname}/components/${argv.family}/${componentName}.jsx`,
+		createIconComponent(componentName, contents, ICON_SIZES[argv.family] || '18')
+	);
+
+	buildIndex(alphabetizedFiles, argv.family);
+});
+
+function buildIndex(files, family) {
+	// initialize empty index file
+	fs.writeFileSync(`${__dirname}/components/${argv.family}/index.js`, '');
+
+	files.forEach(function(file, index) {
 		const camelCaseFilename = toCamelCase(file);
 		const componentName =
 			camelCaseFilename.charAt(0).toUpperCase() + // TitleCasing
 			camelCaseFilename.slice(1).replace('.svg', ''); // stripping .svg extension
-
-		// write component JSX
-		fs.writeFileSync(
-			`${__dirname}/components/${argv.family}/${componentName}.jsx`,
-			createIconComponent(componentName, contents, ICON_SIZES[argv.family] || '18')
-		);
-
-		// append export to index file
-		fs.appendFileSync(
-			`${__dirname}/components/${argv.family}/index.js`,
-			`export { default as ${componentName} } from './${componentName}';\n`
-		);
+		appendToIndex(componentName, family);
 	});
+}
+
+function appendToIndex(component, family) {
+	// append export to index file
+	fs.appendFileSync(
+		`${__dirname}/components/${family}/index.js`,
+		`export { default as ${component} } from './${component}';\n`
+	);
+}
 
 console.log(`finished building ${argv.family} icon set`);
