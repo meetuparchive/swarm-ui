1914c69ff8d11b3e79d60456bba0bf72591be85d
hsbacot <hsbacot@gmail.com>
2020-02-10 17:15:11 -0500

    fix: adding generated components

---
+++ packages/swarm-icons/src/components/large/Linkedin.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Linkedin = props => (
+	<svg data-swarm-icon height="28" width="28" viewBox="0 0 28 28" {...props}>
+		<path d="M21 2H7a5 5 0 00-5 5v14a5 5 0 005 5h14a5 5 0 005-5V7a5 5 0 00-5-5zM10 21H7V10h3v11zM8.5 8.732c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zM22 21h-3v-5.604c0-3.368-4-3.113-4 0V21h-3V10h3v1.765c1.396-2.586 7-2.777 7 2.476V21z" fillRule="nonzero" />
+	</svg>
+);
+
+export default Linkedin;
