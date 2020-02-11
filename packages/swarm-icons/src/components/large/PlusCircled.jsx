bbae90998a4b6c8dd9065611aec6cde97dda4f19
hsbacot <hsbacot@gmail.com>
2020-02-11 10:19:30 -0500

    fix: icons built to components

--- packages/swarm-icons/src/components/large/PlusCircled.jsx
+++ packages/swarm-icons/src/components/large/PlusCircled.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PlusCircled = props => (
 	<svg data-swarm-icon height="28" width="28" viewBox="0 0 28 28" {...props}>
-		<path d="M14 23a9 9 0 100-18 9 9 0 000 18zm0-20c6.075 0 11 4.925 11 11s-4.925 11-11 11S3 20.075 3 14 7.925 3 14 3zm0 5.5a1 1 0 00-1 1V13H9.5a1 1 0 000 2H13v3.5a1 1 0 002 0V15h3.5a1 1 0 000-2H15V9.5a1 1 0 00-1-1z" fillRule="nonzero" />
+		<path d="M14 23a9 9 0 100-18 9 9 0 000 18zm0-20c6.075 0 11 4.925 11 11s-4.925 11-11 11S3 20.075 3 14 7.925 3 14 3zm0 5.5a1 1 0 00-1 1V13H9.5a1 1 0 000 2H13v3.5a1 1 0 002 0V15h3.5a1 1 0 000-2H15V9.5a1 1 0 00-1-1z" />
 	</svg>
 );
 
