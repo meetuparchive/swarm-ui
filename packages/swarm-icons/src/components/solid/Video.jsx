67cd2903b98d7fbbbe8687f65c174762db1c96fa
Kat Guthrie <kguthrie@meetup.com>
2020-04-09 07:34:01 -0700

    fix: runs svg cleanup [#172205984]

--- packages/swarm-icons/src/components/solid/Video.jsx
+++ packages/swarm-icons/src/components/solid/Video.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Video = props => (
 	<svg data-swarm-icon height="18" width="18" viewBox="0 0 18 18" {...props}>
-		<use fillRule="nonzero" />
+		<use />
 	</svg>
 );
 
